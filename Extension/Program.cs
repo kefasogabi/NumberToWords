9609c34853bfd65efa87f7f614170295a98aed2d
kefasogabi <kefasogabi@gmail.com>
2020-05-21 16:58:58 +0100

    Initial

---
+++ Extension/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string more = "yes";
+            Int64 number;
+
+            try
+            {
+                while (more == "yes")
+                {
+                    Console.Write("Please enter a number to convert to words: ");
+                    number = Convert.ToInt64(Console.ReadLine());
+                    var ext = new Ext();
+                    Console.WriteLine(ext.ToWords(number));
+                    Console.Write("do you want to convert again? (Yes/No): ");
+                    more = Convert.ToString(Console.ReadLine()).ToLower();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
